549b9f14424230e637ffdc1908d0b98f78259b91
Carlos Ruiz <carlosruiz9221@gmail.com>
2024-04-05 14:17:03 -0400

    added more buttons and fuxed the js function

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+// function playSound(soundFile) {
+//     var audio = new Audio(soundFile);
+//     audio.play();
+// }
+
+var currentAudio = null; // Variable to store the currently playing audio
+
 function playSound(soundFile) {
+    // Check if there's currently playing audio
+    if (currentAudio !== null) {
+        currentAudio.pause(); // Pause the currently playing audio
+    }
+
     var audio = new Audio(soundFile);
     audio.play();
+    
+    currentAudio = audio; // Set the currently playing audio
 }
